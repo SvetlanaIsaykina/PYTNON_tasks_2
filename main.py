bca564ce8dda82346e20dd4f996fe558344fb209
Svetlana Isaykina <110335993+SvetlanaIsaykina@users.noreply.github.com>
2022-08-19 11:00:08 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,3 @@
+import task_2_1
+import task_2_4
+import task_2_5
---
+++ task_2_1.py
@@ -0,0 +1,8 @@
+a = 15 *3
+print(a, type(a))
+b = 15 / 3
+print(b, type(b))
+c = 15 // 3
+print(c, type(c))
+d = 15 **3
+print(d, type(d))
